a75abe809cebefe6c5b7f26e61488c9325187a67
RahmatJ <rjayanto22.rj@gmail.com>
2022-10-05 11:32:03 +0700

    Add User service and try to insert to db

--- main.go
+++ main.go
@@ -2,46 +2,13 @@ package main
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
 	"go-backer/user"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
-	"net/http"
 )
 
 func main() {
-	//dsn := "root:@tcp(127.0.0.1:3306)/backer_db?charset=utf8mb4&parseTime=True&loc=Local"
-	//db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
-	//if err != nil {
-	//	log.Fatal(err.Error())
-	//}
-	//
-	//fmt.Println("Connected to DB")
-	//
-	//var users []user.User
-	//
-	////gorm automatically detect database and table name
-	//db.Find(&users)
-	//
-	//for _, userData := range users {
-	//	fmt.Println(userData.Name)
-	//	fmt.Println(userData.Email)
-	//	fmt.Println("=============")
-	//}
-
-	router := gin.Default()
-	router.GET("/handler", handler)
-
-	err := router.Run()
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-}
-
-func handler(c *gin.Context) {
 	dsn := "root:@tcp(127.0.0.1:3306)/backer_db?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
@@ -51,10 +18,31 @@ func handler(c *gin.Context) {
 
 	fmt.Println("Connected to DB")
 
-	var users []user.User
+	userRepository := user.NewRepository(db)
+	userService := user.NewService(userRepository)
 
-	//gorm automatically detect database and table name
-	db.Find(&users)
+	userInput := user.RegisterUserInput{
+		Name:       "name from service",
+		Email:      "mail@mail.com",
+		Occupation: "occupation",
+		Password:   "hai",
+	}
 
-	c.JSON(http.StatusOK, users)
+	newUser, err := userService.RegisterUser(userInput)
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	fmt.Println(newUser)
+
+	//router := gin.Default()
+	//router.GET("/handler", handler)
+	//
+	//err := router.Run()
+	//if err != nil {
+	//	log.Fatal(err)
+	//	return
+	//}
 }
