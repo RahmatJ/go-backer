73b7516b5f4ed63927dddb1fb2a3549e1b1cc0ee
RahmatJ <rjayanto22.rj@gmail.com>
2022-10-08 18:57:40 +0700

    Add basic for get campaign detail

--- handler/campaign.go
+++ handler/campaign.go
@@ -29,3 +29,30 @@ func (h *campaignHandler) GetCampaigns(c *gin.Context) {
 	response := helper.APIResponse("List Of Campaigns", http.StatusOK, "success", campaign.FormatCampaigns(campaigns))
 	c.JSON(http.StatusOK, response)
 }
+
+func (h *campaignHandler) GetCampaign(c *gin.Context) {
+	// format endpoints: api/v1/campaign/{ID}
+	var input campaign.GetCampaignDetailInput
+	// handler: mapping id from params url to struct input
+	err := c.ShouldBindUri(&input)
+	if err != nil {
+		response := helper.APIResponse("Failed to get detail of campaign", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	campaign, err := h.service.GetCampaignByID(input)
+	if err != nil {
+		response := helper.APIResponse("Failed to get detail of campaign", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	// service: input using input from user, need to bind first
+	// repository: get campaign by ID
+
+	// handler: mapping result from service to appropriate struct
+
+	response := helper.APIResponse("Campaign Detail", http.StatusOK, "success", campaign)
+	c.JSON(http.StatusOK, response)
+}
